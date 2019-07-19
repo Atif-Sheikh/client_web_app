45f742283f007f0750da5e19e1689e080c7dcb49
Atif-Sheikh <atifsiddiquissg@gmail.com>
2019-07-19 12:02:43 +0500

    Update changes

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { TabContent, Input, InputGroup, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { TabContent, Input, InputGroup, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col,
+   Table, Navbar, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
 import './index.css';
+import history from '../../history';
 
 class Details extends React.Component {
     state = {
-        activeTab: '1'
+        activeTab: '1',
+        value: 'Row Size',
+        data: [],
     };
     
     toggle = (tab) => {
@@ -17,7 +21,21 @@ class Details extends React.Component {
         }
     };
 
+
+  componentDidMount() {
+    this.getData();
+  };
+
+  getData = async () => {
+    let data = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let resp = await data.json();
+    console.log(resp, '>>>>>>>>>>')
+    this.setState({ data: resp });
+  };
+
   render() {
+    const { data } = this.state;
+    let pagenation = data.slice(0, typeof this.state.value === 'string' ? 10 : this.state.value);
       console.log(this.props.match.params, ">>>>>>>>>>>")
     return (
       <div className="containers">
@@ -51,21 +69,53 @@ class Details extends React.Component {
         </Nav>
         <TabContent activeTab={this.state.activeTab}>
           <TabPane tabId="1">
-            <Row>
-              <Col sm="12">
-                <div style={{ display: 'flex', flex: 1, alignItems: 'flex-end', paddingLeft: '20px' }}>
-                    <InputGroup>
-                    <Input placeholder="Search" />
-                    </InputGroup>
-                    <Button style={{ marginTop: '10px' }} color="primary">Search</Button>
-                </div>
-                <div className='cardItem'></div>
-                <div className='cardItem'></div>
-                <div className='cardItem'></div>
-                <div className='cardItem'></div>
-                <Link to='/'> 
+              <Row>
+                <Col sm="12">
+                <Table hover>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Serial Number</th>
+                      <th>Description</th>
+                      <th>Status</th>
+                      <th>Date 1</th>
+                      <th>Date 2</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      pagenation.map((val, ind) => {
+                        return <tr style={{ cursor: 'pointer' }} key={ind}>
+                          <th scope="row">{ind + 1}</th>
+                            <td>{val.title}</td>
+                            <td>{val.body}</td>
+                            <td>user{val.userId}</td>
+                            <td>Otto</td>
+                            <td>Otto</td>
+                        </tr> 
+                      })
+                    }
+                  </tbody>
+                </Table>
+                <Navbar color="light" light expand="md">
+                  <Nav className="ml-auto" navbar>
+                    <UncontrolledDropdown setActiveFromChild>
+                      <DropdownToggle tag="a" className="nav-link" caret>
+                        { this.state.value }
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem onClick={() => this.setState({ value: 10 })}>10</DropdownItem>
+                        <DropdownItem onClick={() => this.setState({ value: 20 })}>20</DropdownItem>
+                        <DropdownItem onClick={() => this.setState({ value: 30 })}>30</DropdownItem>
+                        <DropdownItem onClick={() => this.setState({ value: 40 })}>40</DropdownItem>
+                        <DropdownItem onClick={() => this.setState({ value: 50 })}>50</DropdownItem>
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                  </Nav>
+                </Navbar>
+                <Button className='customBtn' onClick={() => history.push('/')}> 
                     Go Back
-                </Link>
+                </Button>
               </Col>
             </Row>
           </TabPane>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,35 +5,44 @@ import history from '../../history';
 
 class Home extends React.Component {
   state = {
-    value: 'Row Size'
+    value: 'Row Size',
+    data: [],
+    loading: true
   };
   componentDidMount() {
     this.getData();
   };
 
   getData = async () => {
-
+    let data = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let resp = await data.json();
+    console.log(resp, '>>>>>>>>>>')
+    this.setState({ data: resp });
   };
 
   render() {
+    const { data } = this.state;
+    let pagenation = data.slice(0, typeof this.state.value === 'string' ? 10 : this.state.value);
     return (
       <div className="main">
         <div className="leftContainer">
-          <div style={{ display: 'flex', flex: 1, flexDirection: 'column', alignItems: 'flex-end', paddingLeft: '20px' }}>
-            <InputGroup>
-              <Input placeholder="Search" />
-            </InputGroup>
-            <Button style={{ marginTop: '10px' }} color="primary">Search</Button>
-          </div>
-          <div style={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
-            <p style={{ width: '88%', margin: '0 auto', height: '50px' }}>Label 1</p>
-            <p style={{ width: '88%', margin: '0 auto', height: '50px' }}>Label 2</p>
-            <p style={{ width: '88%', margin: '0 auto', height: '50px' }}>Label 3</p>
-          </div>
-          <div style={{ display: 'flex', flex: 1, justifyContent: 'space-around', width: '70%' }}>
-            <div style={true ? { background: 'green' } : { background: 'blue' }} className='circle' />
-            <div style={false ? { background: 'green' } : { background: 'blue' }} className='circle' />
-            <div style={true ? { background: 'green' } : { background: 'blue' }} className='circle' />
+          <div className='wrapper'>
+            <div style={{ display: 'flex', flex: 1, flexDirection: 'column', alignItems: 'flex-end', paddingLeft: '20px' }}>
+              <InputGroup>
+                <Input placeholder="Search" />
+              </InputGroup>
+              <Button style={{ marginTop: '10px' }} color="primary">Search</Button>
+            </div>
+            <div style={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
+              <p style={{ width: '88%', margin: '0 auto', height: '50px' }}>Label 1</p>
+              <p style={{ width: '88%', margin: '0 auto', height: '50px' }}>Label 2</p>
+              <p style={{ width: '88%', margin: '0 auto', height: '50px' }}>Label 3</p>
+            </div>
+            <div style={{ display: 'flex', flex: 1, justifyContent: 'space-around', width: '70%' }}>
+              <div style={true ? { background: 'green' } : { background: 'blue' }} className='circle' />
+              <div style={false ? { background: 'green' } : { background: 'blue' }} className='circle' />
+              <div style={true ? { background: 'green' } : { background: 'blue' }} className='circle' />
+            </div>
           </div>
         </div>
         <div className="rightContainer">
@@ -50,12 +59,12 @@ class Home extends React.Component {
             </thead>
             <tbody>
               {
-                Array.from(new Array(typeof this.state.value === 'string' ? 10 : this.state.value)).map((val, ind) => {
+                pagenation.map((val, ind) => {
                   return <tr onClick={() => history.push(`/details/${ind}`, { state: { index: ind } })} style={{ cursor: 'pointer' }} key={ind}>
                     <th scope="row">{ind + 1}</th>
-                      <td>Mark</td>
-                      <td>Otto</td>
-                      <td>@mdo</td>
+                      <td>{val.title}</td>
+                      <td>{val.body}</td>
+                      <td>user{val.userId}</td>
                       <td>Otto</td>
                       <td>Otto</td>
                   </tr> 
@@ -73,6 +82,8 @@ class Home extends React.Component {
                   <DropdownItem onClick={() => this.setState({ value: 10 })}>10</DropdownItem>
                   <DropdownItem onClick={() => this.setState({ value: 20 })}>20</DropdownItem>
                   <DropdownItem onClick={() => this.setState({ value: 30 })}>30</DropdownItem>
+                  <DropdownItem onClick={() => this.setState({ value: 40 })}>40</DropdownItem>
+                  <DropdownItem onClick={() => this.setState({ value: 50 })}>50</DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
             </Nav>
